1116b25df6024e1794ab227fe9427834e79c3c35
spn2 <sarezh@hotmail.no>
2023-02-02 00:34:56 +0100

    windowing added to cuckoo hash, receive_PRFed_set added

--- Private-Set-Intersection/client_online.py
+++ Private-Set-Intersection/client_online.py
@@ -11,6 +11,129 @@ from oprf_constants import GENERATOR_ORDER, OPRF_CLIENT_KEY
 
 dummy_msg_client = 2 ** (SIGMA_MAX - OUTPUT_BITS + LOG_NO_HASHES)
 
+def main():
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('localhost', 4470))
+
+    HEctx, s_context, s_public_key, s_relin_key, s_rotate_key = client_FHE_setup(POLY_MOD, PLAIN_MOD)
+
+
+    client_to_server_communiation_oprf = send_embedded_items_to_server(client, "client_preprocessed")
+
+    # server tells us how much we will receive for next step
+    bytes_to_receive = int(client.recv(10).decode().strip())
+
+    PRFed_encoded_client_set, server_to_client_communication_oprf = receive_PRFed_set(client, bytes_to_receive)
+
+    t0 = time()
+
+    # We finalize the OPRF processing by applying the inverse of the secret key, oprf_client_key
+    key_inverse = pow(OPRF_CLIENT_KEY, -1, GENERATOR_ORDER)
+    PRFed_client_set = client_prf_online_parallel(PRFed_encoded_client_set, key_inverse)
+
+    print(' * OPRF protocol done!')
+
+    # Each PRFed item from the client set is mapped to a Cuckoo hash table
+    # We pad the Cuckoo vector with dummy messages
+    CH = Cuckoo(HASH_SEEDS)
+    CH.insert_items(PRFed_client_set)
+    CH.pad(dummy_msg_client)
+
+    # We apply the windowing procedure for each item from the Cuckoo structure
+    windowed_items =  CH.windowing(MINIBIN_CAP, PLAIN_MOD)
+
+    plain_query = [None for k in range(len(windowed_items))]
+    enc_query = [[None for j in range(LOG_B_ELL)] for i in range(1, BASE)]
+
+    # We create the <<batched>> query to be sent to the server
+    # By our choice of parameters, number of bins = poly modulus degree (m/N =1), so we get (base - 1) * logB_ell ciphertexts
+    for j in range(LOG_B_ELL):
+        for i in range(BASE - 1):
+            if ((i + 1) * BASE ** j - 1 < MINIBIN_CAP):
+                for k in range(len(windowed_items)):
+                    plain_query[k] = windowed_items[k][i][j]
+                enc_query[i][j] = HEctx.encrypt(plain_query)
+
+    enc_query_serialized = [[None for j in range(LOG_B_ELL)] for i in range(1, BASE)]
+    for j in range(LOG_B_ELL):
+        for i in range(BASE - 1):
+            if ((i + 1) * BASE ** j - 1 < MINIBIN_CAP):
+                enc_query_serialized[i][j] = enc_query[i][j].to_bytes()
+
+
+
+
+
+    # context_serialized = public_context.serialize()
+    # message_to_be_sent = [context_serialized, enc_query_serialized]
+
+    message_to_be_sent = [s_context, s_public_key, s_relin_key, s_rotate_key, enc_query_serialized]
+
+
+    message_to_be_sent_serialized = pickle.dumps(message_to_be_sent, protocol=None)
+    t1 = time()
+
+    L = len(message_to_be_sent_serialized)
+    sL = str(L) + ' ' * (10 - len(str(L)))
+    client_to_server_communiation_query = L 
+    #the lenght of the message is sent first
+    client.sendall((sL).encode())
+    print(" * Sending the context and ciphertext to the server....")
+    # Now we send the message to the server
+    client.sendall(message_to_be_sent_serialized)
+
+    print(" * Waiting for the servers's answer...")
+
+    # The answer obtained from the server:
+    L = client.recv(10).decode().strip()
+    L = int(L, 10)
+    answer = b""
+    while len(answer) < L:
+        data = client.recv(4096)
+        if not data: break
+        answer += data
+    t2 = time()
+    server_to_client_query_response = len(answer) #bytes
+    # Here is the vector of decryptions of the answer
+    ciphertexts = pickle.loads(answer)
+    decryptions = []
+    for ct in ciphertexts:
+        decryptions.append(PyCtxt(bytestring=ct, pyfhel=HEctx, scheme="bfv").decrypt())
+
+    recover_CH_structure = []
+    for matrix in windowed_items:
+        recover_CH_structure.append(matrix[0][0])
+
+    count = [0] * ALPHA
+
+    g = open('client_set', 'r')
+    client_set_entries = g.readlines()
+    g.close()
+    client_intersection = []
+    for j in range(ALPHA):
+        for i in range(POLY_MOD):
+            if decryptions[j][i] == 0:
+                count[j] = count[j] + 1
+
+                # The index i is the location of the element in the intersection
+                # Here we recover this element from the Cuckoo hash structure
+                PRFed_common_element = reconstruct_item(recover_CH_structure[i], i, HASH_SEEDS[recover_CH_structure[i] % (2 ** LOG_NO_HASHES)])
+                index = PRFed_client_set.index(PRFed_common_element)
+                client_intersection.append(int(client_set_entries[index][:-1]))
+
+    h = open('intersection', 'r')
+    real_intersection = [int(line[:-1]) for line in h]
+    h.close()
+    t3 = time()
+    print('\n Intersection recovered correctly: {}'.format(set(client_intersection) == set(real_intersection)))
+    print("Disconnecting...\n")
+    print('  Client ONLINE computation time {:.2f}s'.format(t1 - t0 + t3 - t2))
+    print('  Communication size:')
+    print('    ~ Client --> Server:  {:.2f} MB'.format((client_to_server_communiation_oprf + client_to_server_communiation_query )/ 2 ** 20))
+    print('    ~ Server --> Client:  {:.2f} MB'.format((server_to_client_communication_oprf + server_to_client_query_response )/ 2 ** 20))
+    client.close()
+
 def client_FHE_setup(polynomial_modulus, coefficient_modulus):
     """
     Setting the public and private contexts for the BFV Homorphic Encryption scheme via Pyfhel.
@@ -33,7 +156,7 @@ def client_FHE_setup(polynomial_modulus, coefficient_modulus):
 
     return (HEctx, s_context, s_public_key, s_relin_key, s_rotate_key)
 
-def send_embedded_client_items_to_server(clientsocket, preprocessed_file):
+def send_embedded_items_to_server(clientsocket, preprocessed_file):
     """
     Opens the client's preprocessed set, serializes it and sends it to the server.
 
@@ -55,143 +178,22 @@ def send_embedded_client_items_to_server(clientsocket, preprocessed_file):
 
     return client_to_server_communiation_oprf
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 4470))
+def receive_PRFed_set(clientsocket, bytes_to_receive):
+    """
+    Receives the PRF-processed set from the server.
+    
+    :param clientsocket: client's socket object
+    :param bytes_to_receive: how many bytes we expect to receive from the server
+    """
+    PRFed_encoded_client_set_serialized = b""
+    while len(PRFed_encoded_client_set_serialized) < bytes_to_receive:
+        data = clientsocket.recv(4096)
+        if not data: break
+        PRFed_encoded_client_set_serialized += data   
+    PRFed_encoded_client_set = pickle.loads(PRFed_encoded_client_set_serialized)
+    server_to_client_communication_oprf = len(PRFed_encoded_client_set_serialized)
 
-HEctx, s_context, s_public_key, s_relin_key, s_rotate_key = client_FHE_setup(POLY_MOD, PLAIN_MOD)
+    return PRFed_encoded_client_set, server_to_client_communication_oprf
 
-
-client_to_server_communiation_oprf = send_embedded_client_items_to_server(client, "client_preprocessed")
-
-
-
-
-
-L = client.recv(10).decode().strip()
-L = int(L, 10)
-
-
-
-PRFed_encoded_client_set_serialized = b""
-while len(PRFed_encoded_client_set_serialized) < L:
-    data = client.recv(4096)
-    if not data: break
-    PRFed_encoded_client_set_serialized += data   
-PRFed_encoded_client_set = pickle.loads(PRFed_encoded_client_set_serialized)
-t0 = time()
-server_to_client_communication_oprf = len(PRFed_encoded_client_set_serialized)
-
-# We finalize the OPRF processing by applying the inverse of the secret key, oprf_client_key
-key_inverse = pow(OPRF_CLIENT_KEY, -1, GENERATOR_ORDER)
-PRFed_client_set = client_prf_online_parallel(PRFed_encoded_client_set, key_inverse)
-print(' * OPRF protocol done!')
-
-# Each PRFed item from the client set is mapped to a Cuckoo hash table
-CH = Cuckoo(HASH_SEEDS)
-# for item in PRFed_client_set:
-#     CH.insert(item)
-
-# # We padd the Cuckoo vector with dummy messages
-# for i in range(CH.number_of_bins):
-#     if (CH.data_structure[i] == None):
-#         CH.data_structure[i] = dummy_msg_client
-
-CH.insert_items(PRFed_client_set)
-CH.pad(dummy_msg_client)
-
-# We apply the windowing procedure for each item from the Cuckoo structure
-windowed_items = []
-for item in CH.data_structure:
-    windowed_items.append(windowing(item, MINIBIN_CAP, PLAIN_MOD))
-
-plain_query = [None for k in range(len(windowed_items))]
-enc_query = [[None for j in range(LOG_B_ELL)] for i in range(1, BASE)]
-
-# We create the <<batched>> query to be sent to the server
-# By our choice of parameters, number of bins = poly modulus degree (m/N =1), so we get (base - 1) * logB_ell ciphertexts
-for j in range(LOG_B_ELL):
-    for i in range(BASE - 1):
-        if ((i + 1) * BASE ** j - 1 < MINIBIN_CAP):
-            for k in range(len(windowed_items)):
-                plain_query[k] = windowed_items[k][i][j]
-            enc_query[i][j] = HEctx.encrypt(plain_query)
-
-enc_query_serialized = [[None for j in range(LOG_B_ELL)] for i in range(1, BASE)]
-for j in range(LOG_B_ELL):
-    for i in range(BASE - 1):
-        if ((i + 1) * BASE ** j - 1 < MINIBIN_CAP):
-            enc_query_serialized[i][j] = enc_query[i][j].to_bytes()
-
-
-
-
-
-# context_serialized = public_context.serialize()
-# message_to_be_sent = [context_serialized, enc_query_serialized]
-
-message_to_be_sent = [s_context, s_public_key, s_relin_key, s_rotate_key, enc_query_serialized]
-
-
-message_to_be_sent_serialized = pickle.dumps(message_to_be_sent, protocol=None)
-t1 = time()
-L = len(message_to_be_sent_serialized)
-sL = str(L) + ' ' * (10 - len(str(L)))
-client_to_server_communiation_query = L 
-#the lenght of the message is sent first
-client.sendall((sL).encode())
-print(" * Sending the context and ciphertext to the server....")
-# Now we send the message to the server
-client.sendall(message_to_be_sent_serialized)
-
-print(" * Waiting for the servers's answer...")
-
-# The answer obtained from the server:
-L = client.recv(10).decode().strip()
-L = int(L, 10)
-answer = b""
-while len(answer) < L:
-    data = client.recv(4096)
-    if not data: break
-    answer += data
-t2 = time()
-server_to_client_query_response = len(answer) #bytes
-# Here is the vector of decryptions of the answer
-ciphertexts = pickle.loads(answer)
-decryptions = []
-for ct in ciphertexts:
-    decryptions.append(PyCtxt(bytestring=ct, pyfhel=HEctx, scheme="bfv").decrypt())
-
-recover_CH_structure = []
-for matrix in windowed_items:
-    recover_CH_structure.append(matrix[0][0])
-
-count = [0] * ALPHA
-
-g = open('client_set', 'r')
-client_set_entries = g.readlines()
-g.close()
-client_intersection = []
-for j in range(ALPHA):
-    for i in range(POLY_MOD):
-        if decryptions[j][i] == 0:
-            count[j] = count[j] + 1
-
-            # The index i is the location of the element in the intersection
-            # Here we recover this element from the Cuckoo hash structure
-            PRFed_common_element = reconstruct_item(recover_CH_structure[i], i, HASH_SEEDS[recover_CH_structure[i] % (2 ** LOG_NO_HASHES)])
-            index = PRFed_client_set.index(PRFed_common_element)
-            client_intersection.append(int(client_set_entries[index][:-1]))
-
-h = open('intersection', 'r')
-real_intersection = [int(line[:-1]) for line in h]
-h.close()
-t3 = time()
-print('\n Intersection recovered correctly: {}'.format(set(client_intersection) == set(real_intersection)))
-print("Disconnecting...\n")
-print('  Client ONLINE computation time {:.2f}s'.format(t1 - t0 + t3 - t2))
-print('  Communication size:')
-print('    ~ Client --> Server:  {:.2f} MB'.format((client_to_server_communiation_oprf + client_to_server_communiation_query )/ 2 ** 20))
-print('    ~ Server --> Client:  {:.2f} MB'.format((server_to_client_communication_oprf + server_to_client_query_response )/ 2 ** 20))
-client.close()
-
-
+if __name__ == "__main__":
+    main()
--- Private-Set-Intersection/cuckoo_hash.py
+++ Private-Set-Intersection/cuckoo_hash.py
@@ -4,6 +4,7 @@ from random import randint
 # The hash family used for Cuckoo hashing relies on the Murmur hash family (mmh3)
 import mmh3
 
+from auxiliary_functions import windowing
 from constants import OUTPUT_BITS, NUM_OF_HASHES, NUM_OF_BINS
 
 POW_2_MASK = 2 ** OUTPUT_BITS - 1
@@ -11,90 +12,97 @@ LOG_NO_HASHES = int(math.log(NUM_OF_HASHES) / math.log(2)) + 1
 
 
 def location(seed, item):
-	'''
-	:param seed: a seed of a Murmur hash function
-	:param item: an integer
-	:return: Murmur_hash(item_left) xor item_right, where item = item_left || item_right
-	'''
-	item_left = item >> OUTPUT_BITS
-	item_right = item & POW_2_MASK
-	hash_item_left = mmh3.hash(str(item_left), seed, signed=False) >> (32 - OUTPUT_BITS)
-	return hash_item_left ^ item_right
+    '''
+    :param seed: a seed of a Murmur hash function
+    :param item: an integer
+    :return: Murmur_hash(item_left) xor item_right, where item = item_left || item_right
+    '''
+    item_left = item >> OUTPUT_BITS
+    item_right = item & POW_2_MASK
+    hash_item_left = mmh3.hash(str(item_left), seed, signed=False) >> (32 - OUTPUT_BITS)
+    return hash_item_left ^ item_right
 
 def left_and_index(item, index): 
-	'''
-	:param item: an integer
-	:param index: a LOG_NO_HASHES bits integer
-	:return: an integer represented as item_left || index 
-	'''
-	return ((item >> (OUTPUT_BITS)) << (LOG_NO_HASHES)) + index 	
+    '''
+    :param item: an integer
+    :param index: a LOG_NO_HASHES bits integer
+    :return: an integer represented as item_left || index 
+    '''
+    return ((item >> (OUTPUT_BITS)) << (LOG_NO_HASHES)) + index 	
 
 def extract_index(item_left_and_index): 
-	'''
-	:param item_left_and_index: an integer represented as item_left || index
-	:return: index extracted
-	'''
-	return item_left_and_index & (2 ** LOG_NO_HASHES - 1) 
+    '''
+    :param item_left_and_index: an integer represented as item_left || index
+    :return: index extracted
+    '''
+    return item_left_and_index & (2 ** LOG_NO_HASHES - 1) 
 
 def reconstruct_item(item_left_and_index, current_location, seed):
-	'''
-	:param item_left_and_index: an integer represented as item_left || index
-	:param current_location: the corresponding location, i.e. Murmur_hash(item_left) xor item_right
-	:param seed: the seed of the Murmur hash function
-	:return: the integer item
-	'''
-	item_left = item_left_and_index >> LOG_NO_HASHES
-	hashed_item_left = mmh3.hash(str(item_left), seed, signed=False) >> (32 - OUTPUT_BITS)
-	item_right = hashed_item_left ^ current_location
-	return (item_left << OUTPUT_BITS) + item_right
+    '''
+    :param item_left_and_index: an integer represented as item_left || index
+    :param current_location: the corresponding location, i.e. Murmur_hash(item_left) xor item_right
+    :param seed: the seed of the Murmur hash function
+    :return: the integer item
+    '''
+    item_left = item_left_and_index >> LOG_NO_HASHES
+    hashed_item_left = mmh3.hash(str(item_left), seed, signed=False) >> (32 - OUTPUT_BITS)
+    item_right = hashed_item_left ^ current_location
+    return (item_left << OUTPUT_BITS) + item_right
 
 def rand_point(bound, i): 
-	'''
-	:param bound: an integer
-	:param i: an integer less than bound
-	:return: a uniform integer from [0, bound - 1], distinct from i
-	'''
-	value = randint(0, bound - 1)
-	while (value == i):
-		value = randint(0, bound - 1)
-	return value	
+    '''
+    :param bound: an integer
+    :param i: an integer less than bound
+    :return: a uniform integer from [0, bound - 1], distinct from i
+    '''
+    value = randint(0, bound - 1)
+    while (value == i):
+        value = randint(0, bound - 1)
+    return value	
 
 class Cuckoo():
 
-	def __init__(self, hash_seed):
-		self.number_of_bins = NUM_OF_BINS
-		self.recursion_depth = int(8 * math.log(self.number_of_bins) / math.log(2))
-		self.data_structure = [None for j in range(self.number_of_bins)]
-		self.insert_index = randint(0, NUM_OF_HASHES - 1)
-		self.depth = 0
-		self.FAIL = 0
+    def __init__(self, hash_seed):
+        self.number_of_bins = NUM_OF_BINS
+        self.recursion_depth = int(8 * math.log(self.number_of_bins) / math.log(2))
+        self.data_structure = [None for j in range(self.number_of_bins)]
+        self.insert_index = randint(0, NUM_OF_HASHES - 1)
+        self.depth = 0
+        self.FAIL = 0
 
-		self.hash_seed = hash_seed
+        self.hash_seed = hash_seed
 
-	def insert_items(self, items):
-		for item in items:
-			self.insert(item)
+    def insert_items(self, items):
+        for item in items:
+            self.insert(item)
 
-	def insert(self, item): #item is an integer
-		current_location = location( self.hash_seed[self.insert_index], item)
-		current_item = self.data_structure[ current_location]
-		self.data_structure[ current_location ] = left_and_index(item, self.insert_index)
+    def insert(self, item): #item is an integer
+        current_location = location( self.hash_seed[self.insert_index], item)
+        current_item = self.data_structure[ current_location]
+        self.data_structure[ current_location ] = left_and_index(item, self.insert_index)
 
-		if (current_item == None):
-			self.insert_index = randint(0, NUM_OF_HASHES - 1)	
-			self.depth = 0	
-		else:
-			unwanted_index = extract_index(current_item)
-			self.insert_index = rand_point(NUM_OF_HASHES, unwanted_index)	
-			if (self.depth < self.recursion_depth):
-				self.depth +=1
-				jumping_item = reconstruct_item(current_item, current_location, self.hash_seed[unwanted_index])
-				self.insert(jumping_item)		
-			else:
-				self.FAIL = 1
-	
-	def pad(self, dummy_msg):
-		# pad with dummy_msg
-		for i in range(self.number_of_bins):
-			if (self.data_structure[i] == None):
-				self.data_structure[i] = dummy_msg
+        if (current_item == None):
+            self.insert_index = randint(0, NUM_OF_HASHES - 1)	
+            self.depth = 0	
+        else:
+            unwanted_index = extract_index(current_item)
+            self.insert_index = rand_point(NUM_OF_HASHES, unwanted_index)	
+            if (self.depth < self.recursion_depth):
+                self.depth +=1
+                jumping_item = reconstruct_item(current_item, current_location, self.hash_seed[unwanted_index])
+                self.insert(jumping_item)		
+            else:
+                self.FAIL = 1
+    
+    def pad(self, dummy_msg):
+        # pad with dummy_msg
+        for i in range(self.number_of_bins):
+            if (self.data_structure[i] == None):
+                self.data_structure[i] = dummy_msg
+
+    # apply windowing for all items (self.data_structure), return
+    def windowing(self, minibin_cap, plain_mod):
+        windowed_items = []
+        for item in self.data_structure:
+            windowed_items.append(windowing(item, minibin_cap, plain_mod))
+        return windowed_items
