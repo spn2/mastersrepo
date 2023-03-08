891bac64db9ccafcdf08c1a3ccf4c07ef5002454
spn2 <sarezh@hotmail.no>
2023-03-09 00:19:00 +0100

    set_gen rewritten, "intersection" file added to gitignore

--- Private-Set-Intersection/set_gen.py
+++ Private-Set-Intersection/set_gen.py
@@ -1,23 +1,55 @@
 from random import sample
+from typing import List, Tuple
+
 from constants import SERVER_SIZE, CLIENT_SIZE, INTERSECTION_SIZE
 
-#set elements can be integers < order of the generator of the elliptic curve (192 bits integers if P192 is used); 'sample' works only for a maximum of 63 bits integers.
-disjoint_union = sample(range(2 ** 63 - 1), SERVER_SIZE + CLIENT_SIZE)
-intersection = disjoint_union[:INTERSECTION_SIZE]
-server_set = intersection + disjoint_union[INTERSECTION_SIZE: SERVER_SIZE]
-client_set = intersection + disjoint_union[SERVER_SIZE: SERVER_SIZE - INTERSECTION_SIZE + CLIENT_SIZE]
+def main():
 
-f = open('server_set', 'w')
-for item in server_set:
-	f.write(str(item) + '\n')
-f.close()
+	client_set, server_set, intersection = generate_data_sets(SERVER_SIZE, CLIENT_SIZE, INTERSECTION_SIZE)
 
-g = open('client_set', 'w')
-for item in client_set:
-	g.write(str(item) + '\n')
-g.close()		
+	write_list_to_file("client_set", client_set)
+	write_list_to_file("server_set", server_set)
+	write_list_to_file("intersection", intersection)
 
-h = open('intersection', 'w')
-for item in intersection:
-	h.write(str(item) + '\n')
-h.close()
+
+def generate_data_sets(server_size: int, client_size: int, intersection_size: int) -> Tuple[List[int], List[int]]:
+    """
+    Generates two disjoint sets of integers with the given sizes and intersection size.
+    The integers must be less than the order of the generator of the elliptic curve
+    (e.g., 192-bit integers if P192 is used). Since we use the sample function, we are 
+	limited to 2 ** 63 - 1 (see max_int).
+
+    :param server_size: the size of the set for the server
+    :param client_size: the size of the set for the client
+    :param intersection_size: the size of the intersection between the sets
+    :return: a tuple containing the client set, server set and their intersection
+    """
+
+	# maximum integer allowed for python's sample function
+    max_int = 2 ** 63 - 1
+
+    # Generate a disjoint union of integers for both sets
+    disjoint_union = sample(range(max_int), server_size + client_size)
+
+    # Split the intersection and assign it to the server and client sets
+    intersection = disjoint_union[:intersection_size]
+    server_set = intersection + disjoint_union[intersection_size: server_size + intersection_size]
+    client_set = intersection + disjoint_union[server_size + intersection_size: server_size + client_size]
+
+    return client_set, server_set, intersection
+
+def write_list_to_file(filename: str, items: List) -> None:
+    """
+    Writes the items in the given list to a file with the specified name.
+
+    :param filename: The name of the file to write to.
+    :param items: The list of items to write to the file.
+    :return: None
+    """
+    with open(filename, 'w') as f:
+        for item in items:
+            f.write(str(item) + '\n')
+
+
+if __name__ == "__main__":
+    main()
